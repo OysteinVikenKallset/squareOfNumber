65310ced9a0108318e0363b9d2c305ded86ad81f
Øystein Viken Kallset <34170361+OysteinVikenKallset@users.noreply.github.com>
2024-04-27 21:37:15 +0200

    Squere of number

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int i = 0;
+double userInput = 0;
+bool isValidInput = false;
+
+Console.WriteLine("Skriv inn et tall");
+
+while (!isValidInput)
+{
+    isValidInput = double.TryParse(Console.ReadLine(), out userInput);
+    
+    if (!isValidInput)
+    {
+        Console.WriteLine("Du må skrive inn et tall");
+        isValidInput = false;
+        i++;
+        if (i >= 4)
+        {
+            Console.WriteLine("Du har brukt for mange forsøk");
+            break;
+        }
+    }
+
+} 
+
+if (isValidInput)
+{
+    Console.WriteLine("Kvadratet av tallet er: ");
+    Console.WriteLine(squareOfNumber(userInput));
+}
+
+static double squareOfNumber(double number){
+return number * number;
+}
